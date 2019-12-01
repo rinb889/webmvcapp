7927dba66f9d20b0de43e4696fd1dd6126385064
sagar <sagar@sagar-PC>
2019-12-01 20:31:14 +0530

    customer file

---
+++ src/com/app/Customer.java
@@ -0,0 +1,15 @@
+package com.app;
+
+public class Customer {
+
+	private Integer custId;
+
+	public Integer getCustId() {
+		return custId;
+	}
+
+	public void setCustId(Integer custId) {
+		this.custId = custId;
+	}
+	
+}
